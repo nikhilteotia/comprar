b7013be7e980748c6b3f83ee3ba82c8b545f2ec1
nikhil teotia <50858608+nikhilteotia@users.noreply.github.com>
2020-06-07 19:38:22 +0530

    added images

--- resources/templates/front/carousel.php
+++ resources/templates/front/carousel.php
@@ -9,13 +9,13 @@
                             </ol>
                             <div class="carousel-inner">
                                 <div class="item active">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <img class="slide-image" src="../images/slider/1.jpg"  alt="NOT FOOUND" >
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <img class="slide-image" src="../images/slider/2.jpg" alt="">
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                                    <img class="slide-image" src="../images/slider/3.jpg" alt="">
                                 </div>
                             </div>
                             <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
@@ -28,3 +28,4 @@
                  </div>
 
    </div>
+
